8c8fafaabd5d6c6da1777e7f86edf54010b06ebe
narugo1992 <narugo992@gmail.com>
2024-11-09 03:21:28 +0800

    dev(narugo): init commit

--- aligner/train.py
+++ aligner/train.py
@@ -1,6 +1,6 @@
 import json
 import os
-from typing import Optional
+from typing import Optional, Literal
 
 import numpy as np
 import torch
@@ -22,6 +22,7 @@ from .profile import torch_model_profile
 def train_model(workdir: str, train_dataset: Dataset, test_dataset: Dataset,
                 batch_size: int = 16, max_epochs: int = 100, eval_epoch: int = 1,
                 num_workers: Optional[int] = None, learning_rate: float = 0.001, weight_decay: float = 1e-3,
+                pick_by: str = 'pred_mse', pick_order: Literal['desc', 'asc'] = 'desc',
                 model_name: str = 'simple', suffix_model_name: str = 'SwinV2_v3',
                 seed: int = 0, **options):
     if seed is not None:
@@ -77,6 +78,7 @@ def train_model(workdir: str, train_dataset: Dataset, test_dataset: Dataset,
     model, optimizer, train_dataloader, test_dataloader, scheduler, loss_fn, mse = \
         accelerator.prepare(model, optimizer, train_dataloader, test_dataloader, scheduler, loss_fn, mse)
 
+    best_metric = None
     for epoch in range(1, max_epochs + 1):
         model.train()
         train_lr = scheduler.get_last_lr()[0]
@@ -107,14 +109,16 @@ def train_model(workdir: str, train_dataset: Dataset, test_dataset: Dataset,
         train_sims /= train_total
         train_norms /= train_total
         train_mse /= train_total
-        logging.info(f'Train #{epoch}, LR: {train_lr:.6f}, '
-                     f'loss: {train_loss:.6f}, emb cosine similarity: {train_sims:.6f}, '
-                     f'norm abs error: {train_norms:.6f}, pred mse: {train_mse:.6f}')
-        tb_writer.add_scalar('train/loss', train_loss, epoch)
-        tb_writer.add_scalar('train/emb_cos', train_sims, epoch)
-        tb_writer.add_scalar('train/emb_norms', train_norms, epoch)
-        tb_writer.add_scalar('train/pred_mse', train_mse, epoch)
-        tb_writer.add_scalar('train/learning_rate', train_lr, epoch)
+        train_metrics = {
+            'loss': train_loss,
+            'emb_cos': train_sims,
+            'emb_norms': train_norms,
+            'pred_mse': train_mse,
+            'learning_rate': train_lr,
+        }
+        logging.info(f'Train #{epoch}, metrics: {train_metrics!r}')
+        for key, value in train_metrics.items():
+            tb_writer.add_scalar(f'train/{key}', value, epoch)
 
         if epoch % eval_epoch == 0:
             model.eval()
@@ -138,12 +142,39 @@ def train_model(workdir: str, train_dataset: Dataset, test_dataset: Dataset,
                 test_sims /= test_total
                 test_norms /= test_total
                 test_mse /= test_total
-                logging.info(f'Test #{epoch}, loss: {test_loss:.6f}, emb cosine similarity: {test_sims:.6f}, '
-                             f'norm abs error: {test_norms:.6f}, pred mse: {test_mse:.6f}')
-                tb_writer.add_scalar('test/loss', test_loss, epoch)
-                tb_writer.add_scalar('test/emb_cos', test_sims, epoch)
-                tb_writer.add_scalar('test/emb_norms', test_norms, epoch)
-                tb_writer.add_scalar('test/pred_mse', test_mse, epoch)
+                test_metrics = {
+                    'loss': test_loss,
+                    'emb_cos': test_sims,
+                    'emb_norms': test_norms,
+                    'pred_mse': test_mse,
+                }
+                logging.info(f'Test #{epoch}, metrics: {test_metrics!r}')
+                for key, value in test_metrics.items():
+                    tb_writer.add_scalar(f'test/{key}', value, epoch)
+
+                metric = test_metrics[pick_by]
+                if best_metric is None and (
+                        (pick_order == 'asc' and metric > best_metric) or
+                        (pick_order == 'desc' and metric < best_metric)
+                ):
+                    best_metric = metric
+                    logging.info(f'Epoch #{epoch} selected as the current best epoch, saving best pt...')
+                    torch.save({
+                        'state_dict': model.state_dict(),
+                        'model_options': model_options,
+                        'epoch': epoch,
+                        'train_metrics': train_metrics,
+                        'test_metrics': test_metrics,
+                    }, os.path.join(workdir, 'best.pt'))
+
+                logging.info('Saving last pt...')
+                torch.save({
+                    'state_dict': model.state_dict(),
+                    'model_options': model_options,
+                    'epoch': epoch,
+                    'train_metrics': train_metrics,
+                    'test_metrics': test_metrics,
+                }, os.path.join(workdir, 'last.pt'))
 
 
 if __name__ == '__main__':
